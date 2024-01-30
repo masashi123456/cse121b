ea9ee4da09045989dd0db15600c50fca7b5a42bc
masashi <ota22003@byui.edu>
2024-01-29 20:00:20 -0800

    w2fixed

--- week2/dom-basics.js
+++ week2/dom-basics.js
@@ -1,16 +1,16 @@
-const newParagraph = document.creatElement("p");
-newParagraph.innerText = "Added with Javascript!";
+const newParagraph = document.creatElement('p');
+newParagraph.innerText = 'Added with Javascript!';
 document.body.appendChild(newParagraph);
-const newImage = document.creatElement("img");
-newImage.setAttribute("src", "https://picsum.photo/200");
-newImage.setAttribute("alt", "Image Description")
+const newImage = document.creatElement('img');
+newImage.setAttribute('src', 'https://picsum.photo/200');
+newImage.setAttribute('alt', 'Image Description')
 document.body.appendChild(nnewImage);
-const newSection = document.createElement("section");
-const newh2 = document.createElement("h2");
-newh2.innerText = "CSE 121b";
+const newSection = document.createElement('section');
+const newh2 = document.createElement('h2');
+newh2.innerText = 'CSE 121b';
 newSection.appendChild(newh2);
-const newp = document.createElement("p");
-newp.innerText = "Welcome to Javascript language";
+const newp = document.createElement('p');
+newp.innerText = 'Welcome to Javascript language';
 newSection.appendChild(newp);
 
-document.body.appentChild(sewSection);
+document.body.appentChild(newSection);
