d103ad4147c8744852a16f8a5390f96311c8f532
masashi <ota22003@byui.edu>
2024-02-21 06:28:32 -0800

    finalends

--- scripts/final.js
+++ scripts/final.js
@@ -1,4 +1,5 @@
 const memberInfo = document.getElementById('members');
+const countInfo = document.getElementById('count');
 let memberList = [];
 
 const getMembers = async () =>{
@@ -10,6 +11,22 @@ const getMembers = async () =>{
     }catch(error){
         console.error('error', error)
     }
+
+}
+
+const displayCount = (number, section) =>{
+    let countNumber = document.createElement('p');
+    countNumber.textContent = `The number of ${section} is ${number} `;
+    countInfo.innerHTML = '';
+    countInfo.appendChild(countNumber);
+
+}
+
+const displayCount2 = (number, section) =>{
+    let countNumber = document.createElement('p');
+    countNumber.textContent = `${number} members in this word are not ${section} `;
+    countInfo.innerHTML = '';
+    countInfo.appendChild(countNumber);
 
 }
 
@@ -42,39 +59,52 @@ const filterMembers = (members) =>{
     reset();
     const filter = document.getElementById('filter').value;
     switch(filter){
+        case 'everyone':
+            displayMembers(members);
+            displayCount(members.length, 'the members');
+            break;
         case 'older':
             const older = members.slice().sort ((a,b) => b.age - a.age);
             displayMembers(older);
+            break;
         case 'younger':
             const younger = members.slice().sort ((a,b) => a.age - b.age);
             displayMembers(younger);
+            break;
         case 'ysa':
             const ysa = members.filter(member=>member.age > 17 && member.age < 31 && member.married === 'No') ;
             displayMembers(ysa);
+            displayCount(ysa.length, 'YSA');
             break;
         case 'youth':
             const youth = members.filter(member=>member.age > 11 && member.age < 18) ;
             displayMembers(youth);
+            displayCount(youth.length, 'Youth');
             break;
         case 'primary':
-            const primary = members.filter(member.age < 11) ;
-            displayMembers(prmary);
+            const primary = members.filter(member=>member.age < 11) ;
+            displayMembers(primary);
+            displayCount(primary.length, 'Primary');
             break;
         case 'male':
             const male = members.filter(member=>member.gender === 'Male');
             displayMembers(male);
+            displayCount(male.length, 'brothers');
             break;
         case 'female':
             const female = members.filter(member=>member.gender === 'Female');
             displayMembers(female);
+            displayCount(female.length, 'sisters');
             break;
         case 'married':
             const married = members.filter(member=>member.married === 'No');
             displayMembers(married);
+            displayCount2(married.length, 'married');
             break;
         case 'assignment':
             const assignment = members.filter(member=>member.assignment === 'N/A');
             displayMembers(assignment);
+            displayCount2(assignment.length, 'assigned');
             break;
         
 
